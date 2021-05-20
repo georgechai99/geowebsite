30a8cf072a75bf70ccf014786d62fb0fb9e2adf1
Rebecca Fu <rf382@cornell.edu>
2021-05-20 02:56:34 -0400

    experiment

--- src/components/project.js
+++ src/components/project.js
@@ -1,9 +1,7 @@
 import React from 'react';
-import piece1 from '../images/piece1.jpeg';
 import '../styles/style.css';
 
 function Image(props) {
-    return <img className="portImage" src={ piece1 } alt="portfolio piece 1" />;
   }
 
 export default Image;
